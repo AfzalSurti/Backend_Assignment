c9de591fd20104f21efb52a1b8575578d093a4ae
AfzalSurti <surtishabana35@gmail.com>
2026-04-04 11:01:57 +0530

    db connected succesfully

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 dotenv.config();
 
+const pool = require("./db");
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,18 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Running...");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({
+      message: "Database connection failed",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
