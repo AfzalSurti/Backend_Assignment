5c6888cf2f3eb54bbe0285322a184615736942a2
AfzalSurti <surtishabana35@gmail.com>
2026-04-04 11:23:28 +0530

    put the validation ina  auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const rateLimit = require("express-rate-limit");
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post(
+  "/register",
+  (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    next();
+  },
+  authController.register
+);
+
+// Login rate limiter
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+});
+
+// Login route
+router.post("/login", loginLimiter, authController.login);
 
 module.exports = router;
